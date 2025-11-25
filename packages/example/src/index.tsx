8af605a126f41a87388e47ecaf32be37c09f03e5
anuoua <anuoua@gmail.com>
2025-11-25 20:27:30 +0800

    fix(example): update doc

--- packages/example/src/index.tsx
+++ packages/example/src/index.tsx
@@ -1,8 +1,8 @@
-import { creatRoot } from "j20";
+import { createRoot } from "j20";
 import { App } from "./Todo";
 
 // 用creatRoot创建应用根节点
-const root = creatRoot(() => <App></App>);
+const root = createRoot(() => <App></App>);
 
 // 将根节点添加到DOM中
 document.querySelector("#root")!.append(root.element);
