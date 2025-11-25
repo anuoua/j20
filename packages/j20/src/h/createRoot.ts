23cf6acee98a2fd982ecb943aa9115a520d8a916
anuoua <anuoua@gmail.com>
2025-11-25 06:47:00 +0000

    fix: update createRoot function name

--- packages/j20/src/h/createRoot.ts
+++ packages/j20/src/h/createRoot.ts
@@ -1,8 +1,6 @@
-import { computed } from "../api/computed";
-import { createComponent } from "./createComponent";
 import { instanceCreate } from "./instance";
 
-export const creatRoot = (boot: () => JSX.Element) => {
+export const createRoot = (boot: () => JSX.Element) => {
   let [instance, fragment] = instanceCreate(() => boot());
 
   return {
