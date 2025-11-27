a8f409d615fba81d76866636b18f1272a3a145b7
anuoua <anuoua@gmail.com>
2025-11-27 01:12:08 +0000

    release(vite-plugin): fix type issues in vite-plugin

--- packages/vite-plugin/src/index.ts
+++ packages/vite-plugin/src/index.ts
@@ -1,7 +1,10 @@
 import { signalCompilerRollup } from "signal-compiler/rollup";
 import { j20JsxTransformRollup } from "@j20org/jsx-transform/rollup";
+import type { PluginOption } from "vite";
 
-export function j20({ include = "src/**/*.{js,jsx,ts,tsx}" } = {}) {
+export function j20({
+  include = "src/**/*.{js,jsx,ts,tsx}",
+} = {}): PluginOption {
   return [
     {
       name: "j20",
@@ -18,12 +21,12 @@ export function j20({ include = "src/**/*.{js,jsx,ts,tsx}" } = {}) {
       config: {
         importSource: "j20",
       },
-    }),
+    }) as PluginOption,
     j20JsxTransformRollup({
       include,
       config: {
         importSource: "j20",
       },
-    }),
+    }) as PluginOption,
   ];
 }
