d1e0637c77027ea6b67f39e257a14532c9b6b816
anuoua <anuoua@gmail.com>
2026-01-19 09:47:04 +0000

    release(j20): 0.0.14

--- packages/j20/src/index.ts
+++ packages/j20/src/index.ts
@@ -11,6 +11,7 @@ export * from "./api/untrack";
 export * from "./api/id";
 export * from "./api/onMount";
 export * from "./api/onDestroy";
+export * from "./api/style";
 export * from "./control/For";
 export * from "./control/If";
 export * from "./control/Some";
